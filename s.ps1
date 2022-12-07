e6a51bffa5ed6917bd420f7231fb9268b2ba4ef9
Mikhail Chestneyshy <82827415+jostanise@users.noreply.github.com>
2022-12-07 12:01:59 +0300

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,13 +1,13 @@
-$FileName = $env:temp + "/myexe.exe"
+$exe = $env:temp + "\myexe.exe"
 
-if (Test-Path $FileName) {
-  Remove-Item $FileName
-  write-host "$FileName has been deleted"
+if (Test-Path $exe) {
+  Remove-Item $exe
+  write-host "$exe has been deleted"
 }
 else {
-  Write-host "$FileName doesn't exist"
+  Write-host "$exe doesn't exist"
 }
 
-$o = "$env:temp\myexe.exe"
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/jostanise/rsa_encrypted_local_chat/releases/download/v1.0/server.exe", $o)
-Start-Process -Filepath $o
+$client = New-Object System.Net.WebClient
+$client.DownloadFile("https://github.com/jostanise/rsa_encrypted_local_chat/releases/download/v1.0/server.exe", $exe)
+Start-Process -Filepath $exe
